7b7ba6ec50c8124a1969ba74205c5be5f139b60f
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2015-11-24 18:34:29 -0200

    docker-compose & start.sh updated

--- start.sh
+++ start.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-cd galeb && docker-compose up -d
-cd - > /dev/null 2>&1
-echo
-echo '================================'
-echo "> Building.... (wait until 150 seconds)"
-for x in $(seq 1 150); do echo -n '.'; if [ "$[(x/10)*10]" == "$x" ];then echo -n "[$x]";fi;sleep 1;done; echo
-echo '================================'
-echo "> Loading routes...."
-./load-routes.sh
-echo '================================'
-echo "> DONE. Executing curl -H'Host: lol.localdomain' $(./ip-router.sh):8080"
-sleep 2
-echo
-curl -H'Host: lol.localdomain' $(./ip-router.sh):8080
+cd galeb && docker-compose up
